fe7b301c2b5a33a8434ef7230c939b97e020a275
Dino Reic <rejotl@gmail.com>
2021-09-09 10:16:43 +0200

    added lux controller adapter

--- lib/adapters/view.rb
+++ lib/adapters/view.rb
@@ -6,6 +6,12 @@ elsif Object.const_defined?('Lux::Template::Helper')
   # Lux inject
   Lux::Template::Helper.include ViewCell::ProxyMethod
 
+  Lux::Controller.class_eval do
+    def cell *args
+      ViewCell.cell self.helper, *args
+    end
+  end
+
 elsif Object.const_defined?('Sinatra::Application')
   # Sinatra inject
   Sinatra::Application.include ViewCell::ProxyMethod
